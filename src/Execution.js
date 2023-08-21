7c1e47761a9e59b280008667bb32ad155ae27f03
jswan <jswannieman@gmail.com>
2023-08-21 19:11:11 +1200

    Commit Execution Javascript Component

---
+++ src/Execution.js
@@ -0,0 +1,47 @@
+import { ACTIONS } from "./App";
+
+const divStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+};
+
+export default function Execution({ execution, dispatch4 }) {
+  const listStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    width: "80vw",
+    fontSize: "30px",
+    fontWeight: "bold",
+    color: execution.done ? "navy" : "crimson",
+    fontFamily: execution.done ? "Sofia" : "Tangerine",
+  };
+  return (
+    <div style={divStyle}>
+      <button
+        className="show"
+        onClick={() =>
+          dispatch4({
+            type: ACTIONS.TOGGLE_EXECUTION,
+            stack: { id: execution.id },
+          })
+        }
+      >
+        Toggle
+      </button>
+      <li style={listStyle}>{execution.enteredText}</li>
+      <button
+        className="show"
+        onClick={() =>
+          dispatch4({
+            type: ACTIONS.DELETE_EXECUTION,
+            stack: { id: execution.id },
+          })
+        }
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
